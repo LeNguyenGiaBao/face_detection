acf669d0d760678db1213b0a8ac4d6d085ed7194
lenguyengiabao <lenguyengiabao46@gmail.com>
2022-03-19 10:09:21 +0700

    update bbox for no mask

--- mask_detect_yolov5/app.py
+++ mask_detect_yolov5/app.py
@@ -5,9 +5,13 @@ from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.encoders import jsonable_encoder
 import asyncio
 import uvicorn
+from insightface.app import FaceAnalysis
 
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'custom', './weights/best.pt')  # or yolov5m, yolov5l, yolov5x, custom
+model_mask_detect = torch.hub.load('ultralytics/yolov5', 'custom', './weights/best.pt')  # or yolov5m, yolov5l, yolov5x, custom
+model_name = 'buffalo_m'
+model_face_detect = FaceAnalysis(name=model_name, allowed_modules=['detection']) # enable detection model only
+model_face_detect.prepare(ctx_id=0, det_size=(640, 640))
 
 # Fast API
 app = FastAPI()
@@ -19,6 +23,7 @@ def index():
 @app.post("/detect/")
 async def detect(name_cam: str = Form(""), image: UploadFile = File(None)):
     try:
+        # print(type(image))
         if image == None:
             return jsonable_encoder({
                 "code": 201,
@@ -45,9 +50,10 @@ async def detect(name_cam: str = Form(""), image: UploadFile = File(None)):
             })
 
         # model forward
-        pred = model(img).pred[0]
+        pred = model_mask_detect(img).pred[0]
 
         # assumption main face is the biggest face -> sort by face area
+        # NOTE: save all face 
         pred = sorted(pred, key=lambda x:((x[2]-x[0])*(x[3]-x[1])), reverse=True)[0]
         cls = pred[5].item()
         
@@ -59,11 +65,20 @@ async def detect(name_cam: str = Form(""), image: UploadFile = File(None)):
             }) 
         
         elif cls == 0:  # 0: no mask
+            face_detect = model_face_detect.get(img)
+            print(face_detect[0]['bbox'])
+            x1, y1, x2, y2 = face_detect[0]['bbox']
+            print(x1)
             return jsonable_encoder({
                 "code": 200,
                 "data": 0,
-                "msg": "No Mask"
-            }) 
+                "msg": "No Mask", 
+                'x1': int(x1), 
+                'y1': int(y1),
+                'x2': int(x2), 
+                'y2': int(y2)
+            })
+            # no mask: send result for save image -> face verify
 
     except Exception:
         return jsonable_encoder({
